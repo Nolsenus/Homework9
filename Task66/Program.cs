71780b784cf9f6ce0d93622ed95e43b132c97d2f
Nolsenus <nolsenus@mail.ru>
2022-09-15 15:43:48 +0300

    Initial commit homework done

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿int SumOfNaturalsInRange(int M, int N) {
+    if (N == M) return N;
+    if (N == 1) return 1;
+    return SumOfNaturalsInRange(M, N - 1) + N;
+}
+
+try {
+    Console.Write("Введите первое целое число: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе целое число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"M = {m}; N = {n} -> ");
+    if (m < n) {
+        Console.WriteLine(SumOfNaturalsInRange(m, n));
+    } else {
+        Console.WriteLine(0);
+    }
+} catch (FormatException) {
+    Console.WriteLine("Вы ввели не целое число.");
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,27 @@
+﻿int Ackermann(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    }
+    if (n == 0) {
+        return Ackermann(m - 1, 1);
+    }
+    return Ackermann(m - 1, Ackermann(m, n - 1));  
+}
+
+try {
+    Console.Write("Введите первое неотрицательное целое число: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    if (m >= 0) {
+        Console.Write("Введите второе неотрицательное целое число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n >= 0) {
+            Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {Ackermann(m, n)}");
+        } else {
+            Console.WriteLine("Вы ввели отрицательное число.");
+        }
+    } else {
+        Console.WriteLine("Вы ввели отрицательное число.");
+    }
+} catch (FormatException) {
+    Console.WriteLine("Вы ввели не целое число.");
+}
